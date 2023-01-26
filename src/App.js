e9b44e366463eea23e26518ead248c0b62b61173
mwoolf87 <michael.j.woolf@gmail.com>
2023-01-25 21:46:13 -0500

    added book create form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
-
+import BookCreate from "./components/BookCreate";
+import { useState } from "react";
 const App = () => {
-  return <div>App</div>;
+  const [books, setBooks] = useState([]);
+
+  const createBook = title => {
+    console.log("Need to add book with:", title);
+  };
+  const editBook = () => {};
+  const deleteBook = () => {};
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 };
 
 export default App;

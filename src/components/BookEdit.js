caa48d0149b8b205008b1d725da34850a391b188
mwoolf87 <michael.j.woolf@gmail.com>
2023-01-30 21:14:03 -0500

    completed title edit functionality, added images with picsum.photos

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useState } from "react";
+const BookEdit = ({ book, onSubmit }) => {
+  const [title, setTitle] = useState(book.title);
 
-const BookEdit = () => {
-  return <div>BookEdit</div>;
+  const handleChange = event => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    onSubmit(book.id, title);
+  };
+
+  return (
+    <form className="book-edit" onSubmit={handleSubmit}>
+      <label>Title</label>
+      <input className="input" value={title} onChange={handleChange} />
+      <button className="button is-primary">Save</button>
+    </form>
+  );
 };
 
 export default BookEdit;
--- src/components/Bookshow.js
+++ src/components/Bookshow.js
@@ -1,14 +1,37 @@
 import React from "react";
+import { useState } from "react";
+import BookEdit from "./BookEdit";
 
-const Bookshow = ({ book, onDelete }) => {
-  const handleClick = () => {
+const Bookshow = ({ book, onDelete, onEdit }) => {
+  const [showEdit, setShowEdit] = useState(false);
+
+  const handleDeleteClick = () => {
     onDelete(book.id);
   };
+
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+
+  const handleSubmit = (id, newTitle) => {
+    setShowEdit(false);
+    onEdit(id, newTitle);
+  };
+
+  let content = <h3>{book.title}</h3>;
+  if (showEdit) {
+    content = <BookEdit onSubmit={handleSubmit} book={book} />;
+  }
+
   return (
     <div className="book-show">
-      {book.title}
+      <img src={`https://picsum.photos/seed/${book.id}/300/200`} alt="books" />
+      <div>{content}</div>
       <div className="actions">
-        <button className="delete" onClick={handleClick}>
+        <button className="edit" onClick={handleEditClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={handleDeleteClick}>
           Delete
         </button>
       </div>

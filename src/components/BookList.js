69fea9949d93ae1a0be2bd1ec94d5b58bb2ed6e4
mwoolf87 <mwoolfdev@gmail.com>
2023-02-13 18:02:44 -0500

    added context for better state management

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,12 @@
 import React from "react";
 import BookShow from "./BookShow";
-const BookList = ({ books, onDelete, onEdit }) => {
+import { useContext } from "react";
+import BooksContext from "../context/books";
+const BookList = () => {
+  const { books } = useContext(BooksContext);
+
   const renderedBooks = books.map(book => {
-    return (
-      <BookShow onDelete={onDelete} onEdit={onEdit} key={book.id} book={book} />
-    );
+    return <BookShow key={book.id} book={book} />;
   });
 
   return <div className="book-list">{renderedBooks}</div>;
